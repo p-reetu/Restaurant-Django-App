117fa56d0fa00e87481584352e10275d338fff03
p-reetu <manchekar.preeti@gmail.com>
2025-04-26 14:47:27 +0530

    New Views

--- littlelemon/littlelemon/urls.py
+++ littlelemon/littlelemon/urls.py
@@ -16,8 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from restaurant.views import BookingViewSet
 
+router = DefaultRouter()
+router.register(r'booking', BookingViewSet, basename='booking')
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('restaurant.urls'))
+    path('',include('restaurant.urls')),
+    path('api/',include('rest_framework.urls')),
+    path('restaurant/booking/', include(router.urls)),
 ]
